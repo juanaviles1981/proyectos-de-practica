7af6f98cc6b4b62eb4749a266352c6c307252bff
Juan Aviles <Juanaviles1981@gmail.com>
2025-04-28 13:40:16 -0300

    portfolio-carrito01

--- React/Midu-04-prueba-tecnica/prueba-tecnica/src/App.jsx
+++ React/Midu-04-prueba-tecnica/prueba-tecnica/src/App.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 // Endpoint de la API de gatos
-const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
 // //Endpoint de la imagen de la API de gatos
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true"}`;
 
-export function App() {
-  const [fact, setFact] = useState("cat fact !");
+export function App () {
+  const [fact, setFact] = useState('cat fact !')
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => setFact(data.fact))
   }, [])
 
   return (
