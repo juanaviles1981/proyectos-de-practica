cf5904f08093b0dcf37a20170632946f4ba26cfa
Juan Aviles <Juanaviles1981@gmail.com>
2025-05-19 14:06:14 -0300

    prueba tecnica midu

--- React/Midu-04-prueba-tecnica/prueba-tecnica/src/App.jsx
+++ React/Midu-04-prueba-tecnica/prueba-tecnica/src/App.jsx
@@ -1,51 +1,53 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
 // Endpoint de la API de gatos
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+// Prefijo de la API de gatos
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
-// Endpoint de la imagen de la API de gatos
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true"}`;
-
-//Prefijo de la API de gatos
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
-
-export function App () {
-<<<<<<< HEAD
-  const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
-=======
-  const [fact, setFact] = useState('cargando...')
->>>>>>> fb363181cd04683a7fa4141ec20f721ae97eb605
+export function App() {
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
       .then((data) => {
-        const { fact } = data
-        setFact(fact)
-<<<<<<< HEAD
+        const { fact } = data;
+        setFact(fact);
+      });
+  }, []);
 
-        const threeFirstWords = fact.split(' ', 3).join()
-        
-        fetch(CAT_ENDPOINT_IMAGE_URL)
-          .then((res) => res.json())
-          .then((response) => {
-            const { url } = response
-            setImageUrl(url)
-          })
-      })
-=======
-      }
+  useEffect(() => {
+    if (!fact) return;
 
-      )
->>>>>>> fb363181cd04683a7fa4141ec20f721ae97eb605
-  }, [])
+    const threeFirstWords = fact.split(" ").slice(0, 3).join(" ");
+    
+    fetch(
+      `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        const { url } = response;
+        setImageUrl(url);
+      });
+  }, [fact]);
 
   return (
-    <div>
+    <main>
       <h1>App de gatitos</h1>
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Imagen extraida usando primeras 3 palabras desde ${fact}`} />}
-    </div>
-  )
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt={`Imagen extraida usando primeras 3 palabras desde ${fact}`}
+          />
+        )}
+      </section>
+    </main>
+  );
 }
+
+export default App;
