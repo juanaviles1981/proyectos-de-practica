cf77d8e0acae5f483a221dc6038c5adc116bb3eb
Juan Aviles <Juanaviles1981@gmail.com>
2025-05-27 12:10:22 -0300

    Fin prueba tecnica

--- React/Midu-04-prueba-tecnica/prueba-tecnica/src/App.jsx
+++ React/Midu-04-prueba-tecnica/prueba-tecnica/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useCatImage } from './hooks/useCatImage.js'
 import { useCatFact } from './hooks/useCatFact.js'
 
+
 export function App () {
   const { fact, refreshFact } = useCatFact()
   const { imageUrl } = useCatImage({ fact })
@@ -18,6 +19,7 @@ export function App () {
 
       {fact && <p>{fact}</p>}
       {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+    
     </main>
   )
 }
