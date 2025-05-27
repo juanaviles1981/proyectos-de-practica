b62aa9920087a05faa8648a6e0b3b8c7c4aa9536
Juan Aviles <111673805+juanaviles1981@users.noreply.github.com>
2025-05-27 18:16:48 -0300

    limpieza

--- FullStack/CRUD/src/config.js
+++
@@ -1,7 +0,0 @@
-import {config} from 'dotenv';
-config();
-// console.log(process.env.MONGODB_URI);
-//console.log(process.env.MONGODB_URI);
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/test'; //Si no hay una variable de entorno, se conecta a la base de datos local
-
-export const PORT = process.env.PORT || 3000; //Si no hay una variable de entorno, se conecta al puerto 3000
--- FullStack/CRUD/src/index.js
+++
@@ -1,6 +0,0 @@
-import { PORT } from './config'
-import app from './app'
-import './database'
-app.listen(PORT);
-console.log("server on port", PORT);
-
