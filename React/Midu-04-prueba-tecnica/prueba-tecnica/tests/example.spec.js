d48f1d1e7c07d4cdc0c247f3ad2dccec08a4e920
Juan Aviles Dev <juanaviles1981@gmail.com>
2025-07-17 17:28:58 -0300

    Se agrega test end to end con playwright

--- React/Midu-04-prueba-tecnica/prueba-tecnica/tests/example.spec.js
+++ React/Midu-04-prueba-tecnica/prueba-tecnica/tests/example.spec.js
@@ -1,17 +1,22 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
+import { start } from "repl";
 
-const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 const LOCALHOST_URL = "http://localhost:5173";
+const CAT_FREFIX_IMAGE_URL = "https://catass.com";
 
 test("app shows random fact and image", async ({ page }) => {
   await page.goto(LOCALHOST_URL);
+
   const text = await page.getByRole("paragraph");
+
   const image = await page.getByRole("img");
 
   const textContent = await text.textContent();
-  const imageSrc = await image.getAttribute("src");
 
+  const imageSrc = await image.getAttribute("src");
+  
+  console
   await expect(textContent?.length).toBeGreaterThan(0);
-  await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy();
+  await expect(imageSrc?.startsWith(CAT_FREFIX_IMAGE_URL)).toBeTruthy;
 });
